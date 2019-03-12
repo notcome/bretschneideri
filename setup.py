663b954d45e5a177bd8ddc22e96d265c28be042c
Minsheng Liu <lambda@liu.ms>
2019-03-12 00:10:44 -0700

    Forgot to bump version.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
   name     = 'Bretschneideri',
-  version  = '0.1.3',
+  version  = '0.1.4',
   packages = ['bretschneideri'],
   url      = 'https://github.com/notcome/bretschneideri',
 
