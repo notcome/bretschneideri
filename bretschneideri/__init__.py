4d3849960294c85bfa91c9340395f083994b2449
Minsheng Liu <lambda@liu.ms>
2019-03-12 00:09:22 -0700

    Fix #3.
      Changes to be committed:
      	modified:   bretschneideri/__init__.py

--- bretschneideri/__init__.py
+++ bretschneideri/__init__.py
@@ -4,7 +4,7 @@ from bretschneideri.commandline import cmd_parser
 
 def launch(Task, config: dict = None):
   if config is None:
-    config = cmd_parser.parse_args()
+    config = cmd_parser.parse_args().__dict__
   agent = Agent(Task, **config)
 
   if agent.action == 'train':
